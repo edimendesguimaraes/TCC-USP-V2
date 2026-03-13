f2e0b686a56e51b3b19b865a9d0f1db264f82b36
Edimilson <edimilson.mendes@sinqia.com.br>
2026-03-13 09:14:00 -0300

    inclusao dados cidaddao retorno

---
+++ Zeladoria.Infrastructure/Migrations/20260313121327_AdicionaNomeCidadaoOcorrencia.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Zeladoria.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaNomeCidadaoOcorrencia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NomeCidadao",
+                table: "Ocorrencias",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NomeCidadao",
+                table: "Ocorrencias");
+        }
+    }
+}
